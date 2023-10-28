7b1d16a01509ae716a98215b1615723485ad1305
THIWANKA DULSHAN <88085689+dulshanthilanka@users.noreply.github.com>
2023-10-28 14:56:22 +0530

    Delete dbo.Table.sql

--- dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Books]
-(
-	BookID INT PRIMARY KEY IDENTITY(1,1),
-    Title VARCHAR(255),
-    Author VARCHAR(100),
-    ISBN VARCHAR(20) UNIQUE,
-    PublicationYear INT,
-    Quantity INT
-);
