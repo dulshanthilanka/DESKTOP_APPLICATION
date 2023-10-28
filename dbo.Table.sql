65ad1dfa2897b79307e464723daebb90b68e9ddd
THIWANKA DULSHAN <88085689+dulshanthilanka@users.noreply.github.com>
2023-10-28 14:55:44 +0530

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Books]
+(
+	BookID INT PRIMARY KEY IDENTITY(1,1),
+    Title VARCHAR(255),
+    Author VARCHAR(100),
+    ISBN VARCHAR(20) UNIQUE,
+    PublicationYear INT,
+    Quantity INT
+);
