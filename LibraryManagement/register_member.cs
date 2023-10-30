aae82a5a24d34673d56a66d97cd66f5c09bc3af5
THIWANKA DULSHAN <88085689+dulshanthilanka@users.noreply.github.com>
2023-10-30 23:15:22 +0530

    Update register_member.cs

--- LibraryManagement/register_member.cs
+++ LibraryManagement/register_member.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,65 @@ namespace LibraryManagement
         public register_member()
         {
             InitializeComponent();
+        }
+
+        private void register_member_save_btn_Click(object sender, EventArgs e)
+        {
+            // Get input values from text fields
+            if (int.TryParse(member_id_textfield.Text, out int memberID) &&
+                !string.IsNullOrEmpty(name_of_the_member_textfield.Text) &&
+                !string.IsNullOrEmpty(phone_number_field.Text) &&
+                DateTime.TryParse(register_date_of_the_member_textfield.Text, out DateTime registrationDate))
+            {
+                // Connection string to your database
+                string connectionString = "Data Source=THIWANKA;Initial Catalog=library;Integrated Security=True";
+
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+
+                        // SQL command to insert a new member into the "Members" table
+                        string insertQuery = @"
+                    INSERT INTO Members (MemberID, Name, PhoneNumber, RegistrationDate)
+                    VALUES (@MemberID, @Name, @PhoneNumber, @RegistrationDate);";
+
+                        using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                        {
+                            command.Parameters.AddWithValue("@MemberID", memberID);
+                            command.Parameters.AddWithValue("@Name", name_of_the_member_textfield.Text);
+                            command.Parameters.AddWithValue("@PhoneNumber", phone_number_field.Text);
+                            command.Parameters.AddWithValue("@RegistrationDate", registrationDate);
+
+                            int rowsAffected = command.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("Member added successfully.");
+                                this.Close(); // Close the form
+                            }
+                            else
+                            {
+                                MessageBox.Show("Error adding member.");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid member information.");
+            }
+        }
+
+        private void register_member_cancel_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
